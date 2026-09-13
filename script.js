91714fbfcb48b36b04435135c2ba33e95322054c
severicheandrea9-glitch <severicheandrea9@gmail.com>
2026-09-13 16:17:06 -0500

    Agregar archivos de contabilidad básica corregidos

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarResultado() {
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML =
+        "Resultado: $100.000 = $40.000 + $60.000. La ecuación está equilibrada.";
+}
